sdecastelberg/cakephp

commit 2918ae9c7ab86208be25d5652d94c14685c59faa

Update index.php to serve static files with query strings.
Trim off the query string before attempting to locate the file on the
filesystem. Thanks to Marko Marković for the initial patch.

Fixes #3910

app/webroot/index.php
@@ -79,7 +79,8 @@ if (!defined('WWW_ROOT')) {
 
 // for built-in server
 if (php_sapi_name() == 'cli-server') {
-	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['REQUEST_URI'])) {
+	$uri = str_replace($_SERVER['SCRIPT_FILENAME'], WWW_ROOT, '');
+	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $uri)) {
 		return false;
 	}
 	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);

lib/Cake/Console/Templates/skel/webroot/index.php
@@ -79,7 +79,8 @@ if (!defined('WWW_ROOT')) {
 
 // for built-in server
 if (php_sapi_name() === 'cli-server') {
-	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['REQUEST_URI'])) {
+	$uri = str_replace($_SERVER['SCRIPT_FILENAME'], WWW_ROOT, '');
+	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $uri)) {
 		return false;
 	}
 	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);

commit b6550eb9dd3b923c5490412559fd948040da8823

use PHP_SELF to check for file existence
fixes regression introduced in 2918ae9c7ab86208be25d5652d94c14685c59faa (see #3910)

app/webroot/index.php
@@ -79,8 +79,7 @@ if (!defined('WWW_ROOT')) {
 
 // for built-in server
 if (php_sapi_name() == 'cli-server') {
-	$uri = str_replace($_SERVER['SCRIPT_FILENAME'], WWW_ROOT, '');
-	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $uri)) {
+	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['PHP_SELF'])) {
 		return false;
 	}
 	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);

lib/Cake/Console/Templates/skel/webroot/index.php
@@ -79,8 +79,7 @@ if (!defined('WWW_ROOT')) {
 
 // for built-in server
 if (php_sapi_name() === 'cli-server') {
-	$uri = str_replace($_SERVER['SCRIPT_FILENAME'], WWW_ROOT, '');
-	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $uri)) {
+	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['PHP_SELF'])) {
 		return false;
 	}
 	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);